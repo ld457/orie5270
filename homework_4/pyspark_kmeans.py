fbe00bb5274fc7c03f88ca91e8471292c2999e60
ld457 <31325508+ld457@users.noreply.github.com>
2018-08-29 23:30:35 -0400

    Fixed bugs

--- homework_4/pyspark_kmeans.py
+++ homework_4/pyspark_kmeans.py
@@ -31,8 +31,8 @@ def pyspark_kmeans(data_txt, centroid_txt):
     conf = SparkConf()
     sc = SparkContext(conf=conf)
 
-    data = sc.textFile('data.txt').map(lambda line: np.array([float(x) for x in line.split(' ')])).cache()
-    centroid = sc.textFile('c1.txt').map(lambda line: np.array([float(x) for x in line.split(' ')])).collect()
+    data = sc.textFile(data_txt).map(lambda line: np.array([float(x) for x in line.split(' ')])).cache()
+    centroid = sc.textFile(centroid_txt).map(lambda line: np.array([float(x) for x in line.split(' ')])).collect()
 
     for i in range(max_iter):
 
@@ -44,8 +44,8 @@ def pyspark_kmeans(data_txt, centroid_txt):
         cluster_sum = assigned_data.mapValues(lambda l: (l, 1)).reduceByKey(lambda a, b: (a[0] + b[0], a[1] + b[1]))
         centroid = cluster_sum.map(lambda l: l[1][0] / l[1][1]).collect()
 
-        write_centroid(centroid)
+    write_centroid(centroid)
 
 
 if __name__ == '__main__':
-    pyspark_kmeans(data_txt='A.txt', centroid_txt='c1.txt')
+    pyspark_kmeans(data_txt='data.txt', centroid_txt='c1.txt')
