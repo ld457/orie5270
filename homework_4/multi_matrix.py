31b300e888cf064d2441ee48c8e3b68f6d2e0223
ld457 <31325508+ld457@users.noreply.github.com>
2018-08-30 16:07:40 -0400

    Modified matrix multiplication

--- homework_4/multi_matrix.py
+++ homework_4/multi_matrix.py
@@ -10,17 +10,18 @@ def multi_matrix(a_txt, v_txt):
     data_v = sc.textFile(v_txt)
 
     a = data_a.map(lambda l: [float(i) for i in l.split(',')])
-    v = data_v.map(lambda l: [float(i) for i in l.split(',')])
     a = a.zipWithIndex()
     a = a.map(lambda l: (l[1], [(col, item) for col, item in enumerate(l[0])]))
+    a = a.flatMapValues(lambda l: [i for i in l]).map(lambda l: (l[1][0], (l[0], l[1][1])))
+
+    v = data_v.flatMap(lambda l: [float(i) for i in l.split(',')])
     v = v.zipWithIndex()
     v = v.map(lambda l: (l[1], l[0]))
-    a = a.flatMapValues(lambda l: [i for i in l]).map(lambda l: (l[1][0], (l[0], l[1][1])))
-    v = v.flatMapValues(lambda l: [i for i in l])
 
     av = a.join(v)
     av = av.map(lambda l: (l[1][0][0], l[1][0][1] * l[1][1]))
     av = av.reduceByKey(lambda n1, n2: n1 + n2)
+    av = av.map(lambda l: l[1])
 
     return av
 
